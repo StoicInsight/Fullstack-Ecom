cc61be8c216aa9ca0fcc79ac59c10bdee70b3189
StoicInsight <gettantolcen@gmail.com>
2023-01-18 18:53:57 -0500

    navigation

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { React } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Badge, Box, IconButton } from '@mui/material'
+import {
+  PersonOutline,
+  ShoppingBagOutlined,
+  MenuOutlined, 
+  SearchOutlined
+} from '@mui/icons-material'
+import { useNavigate } from 'react-router-dom'
+import { shades } from '../../theme'
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  
   return (
     <div>Navbar</div>
   )
