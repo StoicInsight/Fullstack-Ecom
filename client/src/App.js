b1a6e1853f6de4bd6914b61a2e632eb52be4b809
StoicInsight <gettantolcen@gmail.com>
2023-01-18 11:44:49 -0500

    server and client

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { 
+  BrowserRouter,
+  Routes, 
+  Route,
+  useLocation
+} from 'react-router-dom'
+import Home from './scenes/home/Home'
+import ItemDetails from './scenes/itemDetails/ItemDetails'
+import Checkout from './scenes/checkout/Checkout'
+
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, [pathname])
+}
+
+const App = () => {
+  
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='item/:itemId' element={<ItemDetails/>}/>
+          <Route path='checkout' element={<Checkout/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
